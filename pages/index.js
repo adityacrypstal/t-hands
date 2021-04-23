fdc85efab2a5b062b8130ec529d62c570b3b8bc7
aditya <adityavadityav@gmail.com>
2021-04-24 00:03:29 +0530

    Testing CI CD

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">T-Hands</a>
+          Welcome to <a href="#">T-Hands</a>
         </h1>
 
         <p className={styles.description}>
