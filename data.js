d45f8f0fa5fd67238736a2099c53acd9f5069fdb
aditya <adityavadityav@gmail.com>
2021-04-30 21:09:53 +0530

    Need to update image size

--- data.js
+++ data.js
@@ -1,29 +1,29 @@
 export const members = [{
     name:"Parvathy J Anil",
     designation:"President",
-    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619795404/samples/members/parvathy_sojejo.jpg"
+    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619796968/samples/members/parvathy_favdo5.jpg"
 },{
     name:"Meghanath",
     designation:"President",
-    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619795404/samples/members/meghanath_vb6vvq.jpg"
+    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619796968/samples/members/meghanath_afhwqo.jpg"
 },{
     name:"Salman",
     designation:"President",
-    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619795404/samples/members/salman_iaczra.jpg"
+    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619796968/samples/members/salman_v1b0iy.jpg"
 },{
     name:"Anand",
     designation:"President",
-    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619795418/samples/members/anand_ykpcl5.png"
+    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619796969/samples/members/anand_h54xmd.png"
 },{
     name:"Ashik",
     designation:"President",
-    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619795404/samples/members/ashik_s11nyi.jpg"
+    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619796968/samples/members/ashik_lrd4os.jpg"
 },{
     name:"Ashik Bin",
     designation:"President",
-    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619795404/samples/members/ashikbin_l4bpmz.jpg"
+    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619796968/samples/members/ashikbin_qkfwkb.jpg"
 },{
     name:"Sooraj",
     designation:"President",
-    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619795404/samples/members/sooraj_gfppar.jpg"
+    image:"https://res.cloudinary.com/t-hands-org/image/upload/v1619796968/samples/members/sooraj_qgg1w7.jpg"
 }];
