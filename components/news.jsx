74922938e1585ec46bf23a4b26a2ce37bd71aea8
aditya <adityavadityav@gmail.com>
2021-05-01 16:23:46 +0530

    Need to update image size

--- components/news.jsx
+++ components/news.jsx
@@ -4,42 +4,40 @@ const News = () => {
     return (
         <section className="letast_news">
             <h2>latest news</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor <br></br> incididunt ut
-                labore et dolore magna aliqua.</p>
+            <p>We aims to provide better living and education to the community. <br></br>Checkout oru recent activities
+                and latest news.</p>
             <div className="container">
                 <div className="row">
                     <div className="col-md-4">
                         <div className="single_news">
-                            <img src="img/news_images_1.jpg" alt="" />
+                            <img src="img/news_images_1.jpg" alt=""/>
                             <div className="texts">
-                                <p className="date"><a href="#">30 May, 2017</a></p>
-                                <h3>Wood Work Adds Value To <br></br> Your Property Five</h3>
-                                <p className="test">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                                <p className="date"><a href="#">01 May, 2021</a></p>
+                                <h3>Blood Donation Campaign<br></br>Covid'19 </h3>
+                                <p className="test">As its not possible to donate blood during vaccination period, we
+                                    conducted a blood donation campaign in Kollam district to fill up blood banks before starting vaccination.</p>
                                 <h3><a href="#">READ MORE</a></h3>
                             </div>
                         </div>
                     </div>
                     <div className="col-md-4">
                         <div className="single_news">
-                            <img src="img/news_images_2.jpg" alt="" />
+                            <img src="img/news_images_2.jpg" alt=""/>
                             <div className="texts">
-                                <p className="date"><a href="#">30 May, 2017</a></p>
-                                <h3>Wood Work Adds Value To <br></br> Your Property Five</h3>
-                                <p className="test">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                                <p className="date"><a href="#">22 April, 2021</a></p>
+                                <h3>Online Photography Contest<br></br> Earth Day</h3>
+                                <p className="test">We conducted an online mobile photography contest based on the theme 'environment' as a part of World Earth Day 2021l </p>
                                 <h3><a href="#">READ MORE</a></h3>
                             </div>
                         </div>
                     </div>
                     <div className="col-md-4">
                         <div className="single_news">
-                            <img src="img/news_images_3.jpg" alt="" />
+                            <img src="img/news_images_3.jpg" alt=""/>
                             <div className="texts">
-                                <p className="date"><a href="#">30 May, 2017</a></p>
-                                <h3>Wood Work Adds Value To <br></br> Your Property Five</h3>
-                                <p className="test">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                                <p className="date"><a href="#">17 April, 2021</a></p>
+                                <h3>Inaguration of T-hands Logo <br></br> T-Hands</h3>
+                                <p className="test">Inaguration of our T-hands logo and Thirikey logo with the presence of out beloved teachers and staff members of JMHS, Bharanikavu.</p>
                                 <h3><a href="#">READ MORE</a></h3>
                             </div>
                         </div>
