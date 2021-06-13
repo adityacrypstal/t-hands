cc73468355cd11b3ce4a4877a60410627a492a09
aditya <adityavadityav@gmail.com>
2021-06-13 23:08:28 +0530

    Added Content

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -20,7 +20,7 @@ const NewsPage = ({data}) => {
                 <Header/>
                 <NextSeo title={post.title}
                          description={post.excerpt}
-                         canonical={url}
+                         canonical={'https://t-hands.vercel.app'}
                          openGraph={{
                              url,
                              title: post.title,
